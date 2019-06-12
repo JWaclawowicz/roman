d62771c2617bc909c5e25a4c997d536e9b419561
Justyna Wacławowicz <justyna.waclawowicz@gmail.com>
2019-06-13 00:34:58 +0200

    Extract RomanConverter component

---
+++ src/RomanConverter.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class RomanConverter extends React.Component {
+    state = {
+        roman: null
+    };
+    handleChange = (event) => {
+        const arabic = event.target.value;
+        let roman = "I";
+        if (arabic == 5) {
+            roman = "V";
+        }
+        this.setState({ roman });
+    }
+    render() {
+        return (
+            <>
+                <label>Arabic:<input onChange={this.handleChange} type="number"></input></label>
+                <h1>Roman: {this.state.roman ? this.state.roman : "none"}</h1>
+            </>
+        );
+    }
+}
+
+export default RomanConverter;
--- src/RomanConverter.test.js
+++ src/RomanConverter.test.js
@@ -1,27 +1,6 @@
 import { cleanup, render, fireEvent } from "@testing-library/react";
 import React from "react";
-
-class RomanConverter extends React.Component {
-    state = {
-        roman: null
-    };
-    handleChange = (event) => {
-        const arabic = event.target.value;
-        let roman = "I";
-        if (arabic == 5) {
-            roman = "V";
-        }
-        this.setState({ roman });
-    }
-    render() {
-        return (
-            <>
-                <label>Arabic:<input onChange={this.handleChange} type="number"></input></label>
-                <h1>Roman: {this.state.roman ? this.state.roman : "none"}</h1>
-            </>
-        );
-    }
-}
+import RomanConverter from "./RomanConverter";
 
 describe('<RomanConverter />', () => {
     afterEach(cleanup);
