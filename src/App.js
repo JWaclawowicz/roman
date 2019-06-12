6f9b17bf21021572f450821f907b049c0371014d
Justyna Wacławowicz <justyna.waclawowicz@gmail.com>
2019-06-13 01:31:49 +0200

    Add RomanConverter component to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RomanConverter from "./RomanConverter";
 
 function App() {
   return (
-    <div />
+    <RomanConverter />
   );
 }
 
