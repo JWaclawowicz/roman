33b9fa47d27acfafe3c6683f6d57f70effcad7cf
Justyna Wacławowicz <justyna.waclawowicz@gmail.com>
2019-06-13 01:10:33 +0200

    Add and skip test for converting 2019

--- src/RomanConverter.test.js
+++ src/RomanConverter.test.js
@@ -24,4 +24,11 @@ describe('<RomanConverter />', () => {
             getByText("Roman: I")
         }).not.toThrow();
     });
+    it.skip('converts 2019 to MMXIX', () => {
+        const { getByLabelText, getByText } = render(<RomanConverter />);
+        fireEvent.change(getByLabelText(/arabic/i), { target: { value: "2019        " }});
+        expect(() => {
+            getByText("Roman: MMXIX")
+        }).not.toThrow();
+    });
 });
