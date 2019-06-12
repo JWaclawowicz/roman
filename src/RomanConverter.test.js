4fcb60aeb8bb2b34271253a569f2fb853f4aa589
Justyna Wacławowicz <justyna.waclawowicz@gmail.com>
2019-06-13 01:05:28 +0200

    Extract toRoman function with tests

--- src/RomanConverter.test.js
+++ src/RomanConverter.test.js
@@ -24,18 +24,4 @@ describe('<RomanConverter />', () => {
             getByText("Roman: I")
         }).not.toThrow();
     });
-    it('converts 5 to V', () => {
-        const { getByLabelText, getByText } = render(<RomanConverter />);
-        fireEvent.change(getByLabelText(/arabic/i), { target: { value: "5" }});
-        expect(() => {
-            getByText("Roman: V")
-        }).not.toThrow();
-    });
-    it('does not convert 0 to any roman number', () => {
-        const { getByLabelText, getByText } = render(<RomanConverter />);
-        fireEvent.change(getByLabelText(/arabic/i), { target: { value: "0" }});
-        expect(() => {
-            getByText("Roman: none")
-        }).not.toThrow();
-    });
 });
